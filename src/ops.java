d3c39aa0f584cfba996c0802e9b67ecdf9c5fb8d
Himneesh <himneesh9@gmail.com>
2022-07-04 23:45:26 +0530

    Initial commit

---
+++ src/ops.java
@@ -0,0 +1,13 @@
+public class ops {
+    static int ret(int a){
+        a=45;
+        return a;
+
+    }
+    public static void main(String[] args) {
+        int x =56;
+        ret(x);
+        System.out.println(x);
+
+    }
+}
